7b76e2df26d781efb5bcd6f9f300f02d9268ef20
조영래 <ip323@naver.com>
2024-06-21 08:53:16 +0900

    파이썬 Print 사용법(1-1) : 이것저것 출력해봐요.

---
+++ python-beginner/chapter02_01.py
@@ -0,0 +1,19 @@
+# Chapter02_01
+# 파이썬 완전 기초
+# Print 사용법
+# 참조 : https://www.python-course.eu/python3_formatted_output.php
+
+# 기본 출력
+print('Python Start!')  # 가장 많이 사용
+print("Python Start!")
+print('''Python Start!''')
+print("""Python Start!""")
+
+print()
+
+# separator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='|')
+print('010', '7777', '1234', sep='-')
+print('python', 'google.com', sep='@')
+
+print()
