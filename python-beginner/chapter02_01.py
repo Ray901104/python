432932201afa1578bbc277e43c4de6aecdc2ad47
yrcho <ip323@naver.com>
2024-06-23 11:22:21 +0900

    파이썬 Print 사용법(1-3) : s, d, f

--- python-beginner/chapter02_01.py
+++ python-beginner/chapter02_01.py
@@ -34,3 +34,35 @@ print()
 print('%s %s' % ('one', 'two',))
 print('{} {}'.format('one', 2))
 print('{1} {0}'.format('one', 'two'))
+
+print()
+
+# %s
+print('%10s' % 'nice')
+print('{:>10}'.format('nice'))
+
+print('%-10s' % 'nice')
+print('{:10}'.format('nice'))
+
+print('{:_>10}'.format('nice'))
+print('{:^10}'.format('nice'))  # 중앙 정렬
+
+print('%.5s' % 'nice')
+print('%.5s' % 'pythonstudy')  # 절삭
+print('{:10.5}'.format('pythonstudy'))
+
+# %d
+print('%d %d' % (1, 2))
+print('{} {}'.format(1, 2))
+
+print('%4d' % 42)
+print('{:4d}'.format(42))
+
+print()
+
+# %f
+print('%f' % 3.143434343434)
+print('{:f}'.format(3.143434343434))
+
+print('%06.2f' % 3.141592653589793)
+print('{:06.2f}'.format(3.141592653589793))
