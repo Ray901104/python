63e389151c3569eec5d7b8cb2f9aa67c75ede182
yrcho <ip323@naver.com>
2024-06-24 18:51:25 +0900

    문자형(2-2)

--- python-beginner/chapter03_02.py
+++ python-beginner/chapter03_02.py
@@ -43,3 +43,48 @@ print()
 raw_s = r'D:\python\test'
 print(raw_s)
 print()
+
+# 멀티 라인 입력
+multi_str = \
+"""
+문자열
+멀티 라인 입력
+테스트
+"""
+
+print(multi_str)
+print()
+
+# 문자열 연산
+str_o1 = "Python"
+str_o2 = "Apple"
+str_o3 = "How are you doing"
+str_o4 = "Seoul Daejeon Busan Jinju"
+
+print(str_o1 * 3)
+print(str_o1 + str_o2)
+print('y' in str_o1)
+print('z' in str_o1)
+print('P' not in str_o2)
+print()
+
+# 문자열 형 변환
+print(str(66), type(str(66)))
+print(str(10.1))
+print(str(True), type(str(True)))
+print()
+
+# 문자열 함수(upper, isalnum, startswith, count, endswith, isal, ...)
+print("Capitalize: ", str_o1.capitalize())  # 첫 글자를 대문자로
+print("endswith?: ", str_o2.endswith("e"))
+print("replace", str_o1.replace("thon", "Good"))
+print("sorted: ", sorted(str_o1))
+print("split: ", str_o4.split(" "))
+print()
+
+# 반복(시퀀스)
+im_str = "Good Boy!"
+print(dir(im_str))
+
+for i in im_str:
+    print(i)
