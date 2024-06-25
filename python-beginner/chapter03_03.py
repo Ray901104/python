34df01bf0d839f0abbdfc3228110ba92e867ce66
조영래 <ip323@naver.com>
2024-06-25 10:39:52 +0900

    리스트(3-2)

--- python-beginner/chapter03_03.py
+++ python-beginner/chapter03_03.py
@@ -32,3 +32,63 @@ print(">>>>>")
 print("c + d", c + d)
 print("c * 3", c * 3)
 print("'Test' + c[0]", "Test" + str(c[0]))
+print()
+
+# 값 비교
+print(c == c[:3] + c[3:])
+print(c)
+print(c[:3] + c[3:])
+print()
+
+# Identity(id)
+temp = c
+print(temp, c)
+print(id(temp))
+print(id(c))
+
+# 리스트 수정
+print(">>>>>")
+c[0] = 4
+print("c - ", c)
+c[1:2] = ['a', 'b', 'c']  # 슬라이싱 -> 리스트로 들어감
+c[1] = ['a', 'b', 'c']  # 원소로 들어감
+print("c - ", c)
+print()
+
+# 리스트 삭제
+c[1:3] = []
+print("c - ", c)
+del c[2]
+print("c - ", c)
+print()
+
+# 리스트 함수
+a = [5, 2, 3, 1, 4]
+print('a - ', a)
+a.append(10)  # 마지막에 삽입
+print('a - ', a)
+a.sort()  # 오름차순 정렬
+print('a - ', a)
+a.reverse()  # 반대로 정렬
+print('a - ', a)
+print('a - ', a.index(3), a[3])
+a.insert(2, 7)  # 중간 삽입
+print('a - ', a)
+a.reverse()
+print('a - ', a)
+a.remove(10)  # 해당 값 삭제
+print('a - ', a)
+print('a - ', a.pop())  # 마지막 원소 삭제
+print('a - ', a)
+print('a - ', a.pop())
+print('a - ', a)
+print('a - ', a.count(4))
+ex = [8, 9]
+a.extend(ex)  # 마지막에 ex 리스트를 붙임
+print('a - ', a)
+
+# 반복문 활용
+while a:
+    data = a.pop()
+    print(data)
+print('a - ', a)
