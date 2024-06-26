5a12b23a46a3d35c63be53246cea51b9da018931
조영래 <ip323@naver.com>
2024-06-26 13:49:25 +0900

    딕셔너리(5-2)

--- python-beginner/chapter03_05.py
+++ python-beginner/chapter03_05.py
@@ -47,3 +47,77 @@ print('b - ', b[0])
 print('b - ', b.get(0))
 print('f - ', f.get('City'))
 print('f - ', f.get('Age'))
+print()
+
+# 딕셔너리 추가
+a['address'] = 'seoul'
+print('a - ', a)
+a['rank'] = [1, 2, 3]
+print('a - ', a)
+print()
+
+# 길이
+print('a - ', len(a))
+print('b - ', len(b))
+print('c - ', len(c))
+print('d - ', len(d))
+print()
+
+# dict_keys, dict_values, dict_items : 반복문(__iter__)에서 사용 가능
+print(a.keys())
+print(b.keys())
+print(c.keys())
+print(d.keys())
+
+print('a - ', list(a.keys()))
+print('b - ', list(b.keys()))
+print()
+
+print(a.values())
+print(b.values())
+print(c.values())
+print('a - ', list(a.values()))
+print('b - ', list(b.values()))
+print()
+
+print('a - ', a.items())
+print('b - ', b.items())
+print('c - ', c.items())
+print('a - ', list(a.items()))
+print('b - ', list(b.items()))
+print()
+
+print('a - ', a.pop('name'))
+print('a - ', a)
+print('c - ', c.pop('arr'))
+print('c - ', c)
+print()
+
+print('f - ', f.popitem())
+print('f - ', f)
+print('f - ', f.popitem())
+print('f - ', f)
+print('f - ', f.popitem())
+print('f - ', f)
+print('f - ', f.popitem())
+print('f - ', f)
+print('f - ', f.popitem())
+print('f - ', f)
+print()
+
+print('a - ', 'birth' in a)
+print('d - ', 'City' in d)
+print()
+
+# 수정
+a['test'] = 'test_dict'
+print('a - ', a)
+a['address'] = 'daejeon'
+print('a - ', a)
+
+a.update(birth='910904')
+print('a - ', a)
+
+temp = {'address': 'Busan'}
+a.update(temp)
+print('a - ', a)
